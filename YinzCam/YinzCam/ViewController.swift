33db4ef1ea5acdc271d806bbc2ff5b57504140c1
kyeoreheo <91kyoheo@gmail.com>
2021-01-11 15:07:11 -0500

    first commit

---
+++ YinzCam/YinzCam/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  YinzCam
+//
+//  Created by Kyo on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
